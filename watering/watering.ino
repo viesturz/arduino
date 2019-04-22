ad8df045f05a9f507806d5e565e7be95e0690996
Viesturs Zariņš <viesturz@gmail.com>
2019-04-22 11:07:11 +0200

    Bug fix

--- watering/watering.ino
+++ watering/watering.ino
@@ -5,7 +5,7 @@ int PIN = 4;
 
 // Arduino has no real time clock, so this should be the exact time when to plug in in minutes.
 const int WATER_DURATION = 10*1000; // ms
-const int START_TIME = 12*60; 
+const int START_TIME = 11*60+10;
 const int SCHEDULE[3] = {9*60, 12*60+1, 18*60};
 
 unsigned long startMillis;
@@ -22,6 +22,7 @@ void loop() {
   int minute = ((millis() - startMillis) / 1000 / 60 + START_TIME) % (24*60);
   if (prevMinute == minute) return; // Still the same minute, skip.
 
+  prevMinute = minute;
   // Water
   for (int i = 0; i <  sizeof(SCHEDULE) / sizeof(SCHEDULE[0]); i ++ ) {
     if (minute == SCHEDULE[i]) water(WATER_DURATION);
